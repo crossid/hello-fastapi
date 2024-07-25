bcaf4448ac610ad147a07724222ab12b4a765bd4
Asaf Shakarzy <asaf@crossid.io>
2024-07-25 14:12:18 +0300

    feat: return env vars names prefixed with HELLO_

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+import os
+
 from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get("/")
 def read_root():
-    return {"message": "Hello, World!"}
+    # list all env vars starting with HELLO-
+    hello_env_vars = {k: v for k, v in os.environ.items() if k.startswith("HELLO_")}
+    return {"message": "Hello, World!", "envs": hello_env_vars}
