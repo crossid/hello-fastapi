99a8c45b3be80e4dbdbce23e09755ce04b98d233
Asaf Shakarzy <asaf@crossid.io>
2024-07-26 12:40:28 +0300

    fix: prefix /api to avoid rewrite rules in ingress

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import os
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 
 app = FastAPI()
 
 
-@app.get("/")
-def read_root():
+@app.get("/api")
+def read_root(request: Request):
     # list all env vars starting with HELLO-
     hello_env_vars = {k: v for k, v in os.environ.items() if k.startswith("HELLO_")}
-    return {"message": "Hello, World!", "envs": hello_env_vars}
+    headers = dict(request.headers)
+    return {"message": "Hello, World!", "envs": hello_env_vars, "headers": headers}
